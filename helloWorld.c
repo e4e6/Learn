99eac85cd95a68540ef1e3c6aee6dff2e1336e04
hangyeol <hangyeol460@gmail.com>
2023-03-20 01:11:46 +0000

    Inital commit

---
+++ helloWorld.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  helloWorld.c
+ *
+ *    Description:  Prints Hello World! 
+ *
+ *        Version:  1.0
+ *        Created:  03/20/23 01:01:50
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  YONSEI UNIV
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, world!\n");
+
+    return 0;
+}
